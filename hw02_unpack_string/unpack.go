ba013235c72f82bb8accb180c7b334cc85d77c4e
fLipE <sergey@ideaweb.site>
2022-11-12 10:18:09 +0200

    hw2 done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,7 +2,6 @@ package hw02_unpack_string
 
 import (
 	"errors"
-	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
@@ -14,17 +13,22 @@ func Unpack(str string) (string, error) {
 	prevExists := false
 
 	for _, s := range str {
-		if unicode.IsDigit(s) {
+
+		isDigit := (s >= 48 && s <= 57)
+		prevIsDigit := (prev >= 48 && prev <= 57)
+
+		if isDigit {
 			if prevExists {
 				for j := 0; j < int(s-'0'); j++ {
 					result = result + string(prev)
 				}
 				prevExists = false
+
 			} else {
 				return "", ErrInvalidString
 			}
 		} else {
-			if !unicode.IsDigit(prev) {
+			if !prevIsDigit {
 				if prevExists {
 					result = result + string(prev)
 				}
